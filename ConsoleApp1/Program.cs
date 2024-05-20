7676b2b17cabb797274fbc0e181c3ffac754580d
MpakaTpak <168774256+MpakaTpak@users.noreply.github.com>
2024-05-20 21:41:02 +0300

    optimiezed

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("MY First Branch");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("MY First Branch");
+        Console.WriteLine("My first scritpt");
+    }
+}
